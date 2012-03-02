d5f64c94d5563fa2a712e748cdc744b2ae972500
dennis <dennis@dennis-Inspiron-531.(none)>
2012-03-02 17:05:51 +0100

    update css

---
+++ compiler.php
@@ -0,0 +1,17 @@
+<?php
+
+$search = array (
+    '{background_color_1}',
+    '{background_color_2}',
+    '{background_main_menu}',
+    
+    
+);
+
+$replace = array (
+    '#fff',
+    '#eee',
+    'url("/templates/gaya/images/nav-bg-bigger.png") repeat-x;',
+    
+    
+);
